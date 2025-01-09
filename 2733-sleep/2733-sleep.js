69fe86ea5faebb62198c5f4bd5f2350a760e15b4
Wfiq <145265758+Fiqqar@users.noreply.github.com>
2025-01-09 14:08:50 +0700

    Time: 56 ms (35.79%), Space: 49.6 MB (9.08%) - LeetHub

---
+++ 2733-sleep/2733-sleep.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+const sleep = async(millis) => await new Promise(resolve => setTimeout(resolve, millis))
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
