63378bb72edc3c9d9d1919bfcf065db49bac60ca
Zulfiqar Silmy Setiawan <145265758+Fiqqar@users.noreply.github.com>
2026-05-31 21:16:24 +0700

    Time: 0 ms (100%), Space: 8.3 MB (77.96%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int a = 0;
+        for(int i = 0;i <stones.size(); i++){
+            for(int j = 0; j<jewels.size(); j++){
+                if(stones[i] == jewels[j]){
+                    a += 1;
+                }
+            }
+        }
+        return a;
+    }
+};
